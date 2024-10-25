b01b280b0ff9ac44bd10f3baf2cf6a51142bc0e7
Saksham Bhardwaj <bhardwajsaksham355@gmail.com>
2024-10-25 10:06:23 +0530

    Adding Bird class

---
+++ core/src/main/java/io/github/saksham355/testgame/bird.java
@@ -0,0 +1,19 @@
+package io.github.saksham355.testgame;
+
+
+    public class bird{
+
+        public String name;
+        public String img;
+        private int impact;
+        public float speed;
+        public void launch(float angle){};
+        public void decreaseImpact(){};
+        public bird(String name, String img) {
+            this.name = name;
+            this.img = img;
+        }
+    }
+
+
+
